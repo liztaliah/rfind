9ae3c88686fb5d86e07ca02ef2720522806329b0
liztaliah <liz.t.hardee@gmail.com>
2024-10-21 12:17:32 -0700

    added recursive functionality

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 use clap::Parser;
 use std::fs;
 use regex::Regex;
+use walkdir::WalkDir;
 
 #[derive(Parser)]
 struct Cli {
-    #[arg(default_value = ".")]
+    #[arg(default_value = ".", short, long)]
     file_path: String,
     query: String,
 }
@@ -12,8 +13,8 @@ struct Cli {
 fn main() {
     let cli = Cli::parse();
     let re = Regex::new(&cli.query).unwrap();
-    let paths = fs::read_dir(cli.file_path).unwrap();
-    for path in paths {
+    // let paths = fs::read_dir(cli.file_path).unwrap();
+    for path in WalkDir::new(cli.file_path) {
         if re.is_match(&path.as_ref().unwrap().path().display().to_string()) {
             println!("{}", path.unwrap().path().display());
         }
